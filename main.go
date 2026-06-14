d5b6744c797c5e305d3afffc749164a6c5444fe8
Shota Hirano <shotahirano@ShotanoMacBook-Air.local>
2026-06-14 12:41:03 +0900

    feat: add root endpoint

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
-func main() { // 👈 ここに { が必要です！
+func main() {
     mysqlUser := os.Getenv("MYSQL_USER")
     mysqlPwd := os.Getenv("MYSQL_PWD")
     mysqlHost := os.Getenv("MYSQL_HOST")
@@ -22,6 +22,12 @@ func main() { // 👈 ここに { が必要です！
         log.Fatal(err)
     }
 
+    // 👇 ここを追加！トップページ (/) にアクセスが来たときのお返事
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Hello Hackathon Backend!")
+    })
+
+    // 既存の /items の処理
     http.HandleFunc("/items", handler.GetItemsHandler(db))
 
     port := os.Getenv("PORT")
